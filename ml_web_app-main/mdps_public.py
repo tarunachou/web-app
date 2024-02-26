d47ab3c9c9713238be1af72955e16e67a9b50844
tarunachou <138558395+tarunachou@users.noreply.github.com>
2024-02-26 10:18:07 +0530

    Update mdps_public.py

--- ml_web_app-main/mdps_public.py
+++ ml_web_app-main/mdps_public.py
@@ -12,11 +12,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('ml_web_app-main/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('ml_web_app-main/heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('ml_web_app-main/parkinsons_model.sav', 'rb'))
 
 
 
